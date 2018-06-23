61bdd1e1037d21963a67359536dbecdae0bfc4c9
Francis Govers <fxgovers@yahoo.com>
2018-06-23 04:17:46 +0000

    added ros interface

--- __init__.py
+++ __init__.py
@@ -1,5 +1,13 @@
 from mycroft import MycroftSkill, intent_file_handler
+#import rospy
+#from std_msgs.msg import String
 
+
+#pub = rospy.Publisher('/syscommand',String,queue_size=10)
+#def publish(pub, str):
+#	pub.publish(str)
+
+#rospy.init_node("listenerCleanRoom")
 
 class CleanRoom(MycroftSkill):
     def __init__(self):
@@ -8,6 +16,7 @@ class CleanRoom(MycroftSkill):
     @intent_file_handler('room.clean.intent')
     def handle_room_clean(self, message):
         self.speak_dialog('room.clean')
+	print("Send Clean Room Command to Robot")
 
 
 def create_skill():
